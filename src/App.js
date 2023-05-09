a7e55bd77ca3cce58d0d93082746e1d84a41af76
Ramsai <ramsaicrony@gmail.com>
2023-05-09 21:32:45 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='login' element={<Login/>} />
+          <Route path='signup' element={<Signup/>}/>
+          <Route path='dashboard' element={<Dashboard/>}/>
+
+      </Routes>
+    </div>);
+}
+
+export default App;
---
+++ src/pages/Signup.js
@@ -0,0 +1,8 @@
+
+const Signup = () => {
+    return (<div>
+        Sign UPP
+    </div>);
+}
+
+export default Signup;
